b39e639863379a510fc79eee2ad3d115bd6d5a70
Colin Cannell <92816299+colin-cannell@users.noreply.github.com>
2023-11-20 10:41:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# token = MTE3NTkwNDY3NjczMDkxMjkyMQ.GZjGoh.zTR4iN_nYBZZcmnVBGpJPh6tUa2ssx3cQp3gEM
 # invite link = https://discord.com/api/oauth2/authorize?client_id=1175904676730912921&permissions=1084479765568&scope=bot
 
 import bot
